58df8ac16e42ea615c6b33eea8b4e49787a44bd6
frosipedro <pedrorfrosi@gmail.com>
2024-07-08 13:30:43 -0300

    docs: added swagger documentation

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import dotenv from 'dotenv';
 import express from 'express';
 import connectDB from './database/connection';
 import routes from './routes/index';
+import swaggerUi from 'swagger-ui-express';
+import swaggerFile from './swagger/swagger_output.json';
 
 const app = express();
 dotenv.config();
@@ -9,9 +11,12 @@ dotenv.config();
 connectDB();
 app.use(express.json());
 
+app.use('/api/v1/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(routes);
 
 const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+export default app;
